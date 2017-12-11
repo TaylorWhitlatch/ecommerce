72d25801a0969108b19bff0f78fc6b39c75b00d2
whit121 <Taylorwhitlatch112@gmail.com>
2017-12-11 12:28:57 -0500

    2

---
+++ front-end/src/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+	constructor(){
+		super();
+
+	}
+
+	render(){
+		return(
+			<nav>NavBar Check</nav>
+		)
+	}
+}
+
+
+export default NavBar
